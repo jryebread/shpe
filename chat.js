ce3035b1cce51edd4f9e0a97c92ed7c7da6531a5
James Pog <jamesryebread@gmail.com>
2023-04-14 21:47:25 -0700

    Update chat.js

--- chat.js
+++ chat.js
@@ -86,7 +86,7 @@ chat.style.width = '85vw'
 chat.style.height = '70vh'
 chat.style.boxShadow =
   'rgba(150, 150, 150, 0.15) 0px 6px 24px 0px, rgba(150, 150, 150, 0.15) 0px 0px 0px 1px'
-// chat.style.display = 'none'
+chat.style.display = 'none'
 chat.style.borderRadius = '10px'
 chat.style.zIndex = 999999999
 chat.style.overflow = 'hidden'
